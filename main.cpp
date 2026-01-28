52e76dad208c619659eabfbd5e8cac76c2b0cef3
benbergensten <benjamin.bergensten@student.northampton.edu>
2026-01-28 18:55:38 +0000

    Wrote BMI Program

--- main.cpp
+++ main.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 int main() {
- 
+    int height = 61;
+    int weight = 130; 
+    
+    double kg, meters, BMI;
+    kg = weight*0.453592; //Wieght conversion  
+    meters = height*0.0254; //Height conversion
+    BMI = kg/(meters*meters); //BMI calculation
+    cout << "The BMI for this person is: " << BMI << endl;
+    
+
+
 
 
 }
